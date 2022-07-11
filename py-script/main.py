9f8d7daaff3eb1f6dd2831c9c50d4323f34121c6
svan9 <75327234+svan9@users.noreply.github.com>
2022-07-12 01:02:20 +0300

    <built-in method random of Random object at 0x000001AD65F3A8B0>

--- py-script/main.py
+++ py-script/main.py
@@ -18,6 +18,7 @@ json.dump(json_inner, list_txt)
 os.system("cd C:/Users/ivans/OneDrive/Документы/GitHub/PRG")
 for i in os.listdir(os.path.abspath("C:/Users/ivans/OneDrive/Документы/GitHub/PRG")):
     os.system("git add " + i)
-    
-os.system(f"git commit -m \"{1213}\"")
+
+os.system(f"git commit -m \"{random.random}\"")
 os.system(f"git push")
+print(random.random)
