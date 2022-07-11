f43f36788c2acfa4c8c52de8a4cf23a2a0ef89e7
svan9 <75327234+svan9@users.noreply.github.com>
2022-07-12 01:30:39 +0300

    1464385079988792

--- py-script/main.py
+++ py-script/main.py
@@ -17,8 +17,8 @@ json.dump(json_inner, list_txt)
 
 os.system("cd C:/Users/ivans/OneDrive/Документы/GitHub/PRG")
 for i in os.listdir(os.path.abspath("C:/Users/ivans/OneDrive/Документы/GitHub/PRG")):
-    if i not in ("main.scss", "main.py", "start.bat"):
-        os.system("git add " + i)
+    # if i not in ("main.scss", "main.py", "start.bat"):
+    os.system("git add " + i)
 
 os.system(f"git commit -m \""+str(random.random()).split(".")[-1][::-1]+"\"")
 os.system(f"git push")
